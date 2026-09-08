175b1df28ba805fe93cdcc8e5e7ade4c606ba6fe
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-09-08 16:14:07 +0100

    Delete compile.sh

--- compile.sh
+++
@@ -1,6 +0,0 @@
-printf "\033c\033[47;30m\ngive a xml file to compile\n"
-read a
-printf "\033[47;30m\ngive a pas lib file to compile\n"
-read b
-
-fpdoc --package=$a --input=$b --descr=$a --format=chm --output=$a.chm
